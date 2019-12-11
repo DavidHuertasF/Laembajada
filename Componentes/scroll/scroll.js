33d58622dab16a0a020c8e24f200934db45fd770
DavidHuertasF <david.huertasf@hotmail.com>
2019-12-11 18:32:48 -0500

    :100:

--- Componentes/scroll/scroll.js
+++ Componentes/scroll/scroll.js
@@ -1,3 +1,5 @@
+menu();
+
 $(window).scroll(function() {
   var $righta = $("#scroll-component-right-a");
   var $rightb = $("#scroll-component-right-b");
@@ -9,7 +11,7 @@ $(window).scroll(function() {
   var $divblue = $(".div-blue");
 
   var windowHeight = $(window).scrollTop();
-
+ 
 
   $divblue.css({
     "top": ((-windowHeight / 100) + 14) *2 + "%"
@@ -40,9 +42,72 @@ $(window).scroll(function() {
     "margin-top": windowHeight / 100 + 32 + "%"
   });
 
+menu();
+
 
 
 });
 
+function refresh(){
+  var es = document.getElementsByClassName("l-e"); // get all elements
+  for (var i = 0; i < es.length; i++) {
+    es[i].style.display = "none";
+  }
+  var es = document.getElementsByClassName("r-c"); // get all elements
+  for (var i = 0; i < es.length; i++) {
+    es[i].style.display = "none";
+  }
+  var es = document.getElementsByClassName("c-j"); // get all elements
+  for (var i = 0; i < es.length; i++) {
+    es[i].style.display = "none";
+  }
+  var es = document.getElementsByClassName("p-c"); // get all elements
+  for (var i = 0; i < es.length; i++) {
+    es[i].style.display = "none";
+  }
+  var es = document.getElementsByClassName("n-g"); // get all elements
+  for (var i = 0; i < es.length; i++) {
+    es[i].style.display = "none";
+  }
+}
+
+function menu(){
+  var windowHeight = $(window).scrollTop();
+  var sec = ((windowHeight*100)/window.innerHeight );
+  if(sec<90){
+    refresh();
+  }else if(sec<180){
+    refresh();
+    var es = document.getElementsByClassName("l-e"); // get all elements
+      for (var i = 0; i < es.length; i++) {
+        es[i].style.display = "contents";
+      }
+  }else if(sec<270){
+    refresh();
+    var es = document.getElementsByClassName("r-c"); // get all elements
+      for (var i = 0; i < es.length; i++) {
+        es[i].style.display = "contents";
+      }
+  }else if(sec<350){
+    refresh();
+    var es = document.getElementsByClassName("c-j"); // get all elements
+      for (var i = 0; i < es.length; i++) {
+        es[i].style.display = "contents";
+      }
+  }else if(sec<420){
+    refresh();
+    var es = document.getElementsByClassName("p-c"); // get all elements
+      for (var i = 0; i < es.length; i++) {
+        es[i].style.display = "contents";
+      }
+  
+}else{
+  refresh();
+  var es = document.getElementsByClassName("n-g"); // get all elements
+    for (var i = 0; i < es.length; i++) {
+      es[i].style.display = "contents";
+    }
+  }
+}
 
 
