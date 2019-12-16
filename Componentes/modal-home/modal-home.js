558ecce24b44f4f26f372da5cb7ddafcc5710d43
DavidHuertasF <david.huertasf@hotmail.com>
2019-12-16 17:04:23 -0500

    :+1:

--- Componentes/modal-home/modal-home.js
+++ Componentes/modal-home/modal-home.js
@@ -1,20 +1,31 @@
 // Get the modal
 var modal = document.getElementById("myModal");
+var modalM = document.getElementById("myModal-m");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
+var btnM = document.getElementById("myBtn-m");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
+var spanM = document.getElementsByClassName("close-m")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
+btnM.onclick = function() {
+  modalM.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+
+spanM.onclick = function() {
+  modalM.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -24,20 +35,30 @@ window.onclick = function(event) {
   }
 }
 
+window.onclick = function(event) {
+  if (event.target == modalM) {
+    modalM.style.display = "none";
+  }
+}
+
 
 var sectionA =  document.getElementById("section-a");
 var sectionB =  document.getElementById("section-b");
 var sectionC =  document.getElementById("section-c");
 
-sectionA.onclick = function() {
-    document.getElementById('pop-section').src='img/svg/pop-sections/s1.svg'
-    document.getElementById('galery-modal').src='img/pop-sections/s1.png'
+var sectionAm =  document.getElementById("section-a-m");
+var sectionBm =  document.getElementById("section-b-m");
+var sectionCm =  document.getElementById("section-c-m");
+
+sectionAm.onclick = function() {
+    document.getElementById('pop-section-m').src='img/svg/pop-sections/s1.svg'
+    document.getElementById('galery-modal-m').src='img/pop-sections/s1.png'
 }
-sectionB.onclick = function() {
-    document.getElementById('pop-section').src='img/svg/pop-sections/s2.svg'
-    document.getElementById('galery-modal').src='img/pop-sections/s2.png'
+sectionBm.onclick = function() {
+    document.getElementById('pop-section-m').src='img/svg/pop-sections/s2.svg'
+    document.getElementById('galery-modal-m').src='img/pop-sections/s2.png'
 }
-sectionC.onclick = function() {
-    document.getElementById('pop-section').src='img/svg/pop-sections/s3.svg'
-    document.getElementById('galery-modal').src='img/pop-sections/s3.png'
+sectionCm.onclick = function() {
+    document.getElementById('pop-section-m').src='img/svg/pop-sections/s3.svg'
+    document.getElementById('galery-modal-m').src='img/pop-sections/s3.png'
 }
